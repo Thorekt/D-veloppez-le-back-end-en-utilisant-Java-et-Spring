0f2d05a37afa3fca4ee93c843dc0bcf0763ee65b
rory <35566455+Thorekt@users.noreply.github.com>
2025-09-15 16:49:50 +0200

    core: ecriture des models en ce basant sur le bdd

--- src/main/java/com/thorekt/chatop/models/Message.java
+++ src/main/java/com/thorekt/chatop/models/Message.java
@@ -1,5 +1,60 @@
 package com.thorekt.chatop.models;
 
-public class Message {
+import java.sql.Timestamp;
 
+public class Message {
+    private int id;
+    private int rentalId;
+    private int userId;
+    private String message;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRentalId() {
+        return rentalId;
+    }
+
+    public void setRentalId(int rentalId) {
+        this.rentalId = rentalId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
--- src/main/java/com/thorekt/chatop/models/Rental.java
+++ src/main/java/com/thorekt/chatop/models/Rental.java
@@ -1,5 +1,87 @@
 package com.thorekt.chatop.models;
 
-public class Rental {
+import java.sql.Timestamp;
 
+public class Rental {
+    private int id;
+    private String name;
+    private double surface;
+    private double price;
+    private String picture;
+    private String description;
+    private int ownerId;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSurface() {
+        return surface;
+    }
+
+    public void setSurface(double surface) {
+        this.surface = surface;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
