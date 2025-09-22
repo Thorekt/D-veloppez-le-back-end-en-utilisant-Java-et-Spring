7b8e6da12cfa68b114c0d0b43f2b65846e697551
rory <35566455+Thorekt@users.noreply.github.com>
2025-09-22 16:07:50 +0200

    feat: ajout d'un enpoint permettant l''enregistrement de message

--- src/main/java/com/thorekt/chatop/controller/MessageController.java
+++ src/main/java/com/thorekt/chatop/controller/MessageController.java
@@ -1,5 +1,41 @@
 package com.thorekt.chatop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thorekt.chatop.dto.request.MessageRequest;
+import com.thorekt.chatop.dto.response.BaseResponse;
+import com.thorekt.chatop.service.MessageService;
+
+/**
+ * Controller for message management
+ * 
+ * @author thorekt
+ */
+@RestController
 public class MessageController {
 
+    @Autowired
+    private MessageService messageService;
+
+    /**
+     * Post a new message
+     * 
+     * @param request
+     * @return BaseResponse indicating success or failure
+     */
+    @PostMapping("/messages")
+    public ResponseEntity<BaseResponse> postMessage(@RequestBody MessageRequest request) {
+
+        try {
+            messageService.createMessage(request.message(), request.userId(), request.rentalId());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new BaseResponse(e.getMessage()));
+        }
+
+        return ResponseEntity.ok(new BaseResponse("Message send with success"));
+    }
 }
